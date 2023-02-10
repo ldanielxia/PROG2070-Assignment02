9133d8047160f0e00507a6b261ec6fca15d39f15
Xia(Daniel) Li <ldanielxia@yahoo.com>
2023-02-09 23:56:51 -0500

    First test category finished
    Only one (1) test for a valid equilateral triangle

---
+++ TriangleSolver/TriangleSolverTest/TriangleTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+
+namespace TriangleSolverTest
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        //Only one (1) test for a valid equilateral triangle
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidEquilateralTriangle()
+        {
+            //Arrange
+            int firstAngel = 60;
+            int secondAngel = 60;
+            int thirdAngle = 60;
+
+            string expected = "Based on all sides being equal, the type of triangle is an EQUILATERAL";
+            //Art
+            String actual = new Triangle().AnalyzeTriangle(firstAngel, secondAngel, thirdAngle);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
